54d37d8f137fb0813409ffff3ce975d3b9ee485c
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2022-04-24 14:39:39 +0200

    final result textures explained

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Threeveloper
 
-Hey everyone, this branch contains the base code based on react v18 and @react-three/fiber v8 which recently came out.
+Hey everyone, here you can find the final code for the video `React Three Fiber & Three.js Materials Explained`. It includes all the textures that have been used in the code as well, which I downloaded from [Poly Haven](https://polyhaven.com/) and edited for demoing purposes.
 
 The source code for each videos is seperated into it's own branch. The link to the branch is added to the description of that video.
--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,97 @@
+import { OrbitControls, Plane, useHelper, useTexture } from "@react-three/drei";
 import {
-  Icosahedron,
-  OrbitControls,
-  Plane,
-  Sphere,
-  useGLTF,
-  useHelper,
-  useTexture,
-} from "@react-three/drei";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+  Canvas,
+  MeshStandardMaterialProps,
+  useThree,
+} from "@react-three/fiber";
+import { useControls } from "leva";
 import { useEffect, useRef } from "react";
 import {
+  BufferAttribute,
   DoubleSide,
   LinearEncoding,
+  Mesh,
   PointLight,
   PointLightHelper,
-  SpotLightHelper,
   Vector2,
-  Vector3,
 } from "three";
 import "./App.css";
 
-function RockSphere() {
-  const groundProps = useTexture({
+function Terrain() {
+  const terrainTextures = useTexture({
     map: "/textures/aerial_rocks_02_diff_4k.jpg",
-    aoMap: "/textures/aerial_rocks_02_arm_4k.jpg",
-    roughnessMap: "/textures/aerial_rocks_02_arm_4k.jpg",
-    metalnessMap: "/textures/aerial_rocks_02_arm_4k.jpg",
-    displacementMap: "/textures/aerial_rocks_02_disp_4k.jpg",
+    aoMap: "/textures/aerial_rocks_02_arm_4k_ao_edited.jpg",
+    roughnessMap: "/textures/aerial_rocks_02_arm_4k_roughness_edited.jpg",
+    metalnessMap: "/textures/aerial_rocks_02_arm_4k_metalness_edited.jpg",
+    displacementMap: "/textures/aerial_rocks_02_disp_4k_edited.jpg",
     alphaMap: "/textures/alpha.png",
-    normalMap: "/textures/aerial_rocks_02_nor_gl_4k.jpg",
+    normalMap: "/textures/aerial_rocks_02_nor_gl_4k_edited.jpg",
+  });
+
+  const { displacementScale, aoMapIntensity, normalScale } = useControls({
+    displacementScale: {
+      value: 1,
+      min: -2,
+      max: 2,
+    },
+    aoMapIntensity: {
+      value: 1,
+      min: 0,
+      max: 10,
+    },
+    normalScale: [1, 1],
   });
 
   const { camera } = useThree();
   useEffect(() => {
-    camera.position.set(-5, 10, 0);
+    camera.position.set(0, 10, 5);
+  }, [camera]);
+
+  const planeProps: {
+    args: [
+      width: number,
+      height: number,
+      widthSegments: number,
+      heightSegments: number
+    ];
+    rotation: [x: number, y: number, z: number];
+  } = {
+    args: [10, 10, 128, 128],
+    rotation: [-Math.PI / 2, 0, 0],
+  };
+
+  const materialProps: MeshStandardMaterialProps = {
+    ...terrainTextures,
+    side: DoubleSide,
+    transparent: true,
+    displacementScale,
+    aoMapIntensity,
+    normalScale: new Vector2(normalScale[0], normalScale[1]),
+  };
+
+  const mesh = useRef<Mesh>(null!);
+  useEffect(() => {
+    mesh.current.geometry.setAttribute(
+      "uv2",
+      new BufferAttribute(mesh.current.geometry.attributes.uv.array, 2)
+    );
   });
 
   return (
     <>
-      <Plane args={[10, 10, 128, 128]} rotation={[-Math.PI / 2, 0, 0]}>
-        <meshStandardMaterial
-          side={DoubleSide}
-          transparent
-          {...groundProps}
-          normalMap-encoding={LinearEncoding}
-        />
+      <Plane {...planeProps} ref={mesh}>
+        <meshStandardMaterial {...materialProps} />
       </Plane>
-      <Plane
-        args={[10, 10, 128, 128]}
-        position={[10, 0, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-      >
-        <meshStandardMaterial
-          side={DoubleSide}
-          transparent
-          {...groundProps}
-          normalMap-encoding={LinearEncoding}
-        />
+      <Plane {...planeProps} position={[10, 0, 0]}>
+        <meshStandardMaterial {...materialProps} />
       </Plane>
-      <Plane
-        args={[10, 10, 128, 128]}
-        position={[10, 0.1, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-      >
+      <Plane {...planeProps} position={[10, 0.1, 0]}>
         <meshStandardMaterial
+          {...materialProps}
           wireframe
-          {...groundProps}
           color={"white"}
           map={null}
+          normalMap-encoding={LinearEncoding}
         />
       </Plane>
     </>
@@ -84,12 +108,12 @@ function ThreeContent() {
       <pointLight
         ref={lightRef}
         position={[5, 5, 0]}
-        intensity={2}
+        intensity={4}
         color={"white"}
       />
       <OrbitControls />
 
-      <RockSphere />
+      <Terrain />
     </>
   );
 }
