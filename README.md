f75bc4e64eacdf607710806d88db252f172237ff
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2022-10-04 19:49:42 +0200

    explain environment maps

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Threeveloper
 
-Hey everyone, this branch contains the base code based on react v18 and @react-three/fiber v8 which recently came out.
-
-The source code for each videos is seperated into it's own branch. The link to the branch is added to the description of that video.
+Hey there, 3D web developer! 👋 I hope you enjoyed this video about environment maps. Here you can find the final result of that video. If you have any questions, feel free to comment them down below the YouTube video in the comment section!
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,86 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import {
+  Box,
+  CubeCamera,
+  Environment,
+  OrbitControls,
+  Sphere,
+  useEnvironment,
+} from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useControls } from "leva";
+import { ReactNode, useRef } from "react";
+import { Group } from "three";
 import "./App.css";
 
-function ThreeScene() {
+function Rotator({ children }: { children: ReactNode }) {
+  const groupRef = useRef<Group>(null!);
+
+  useFrame(() => {
+    groupRef.current.children.forEach((child) => {
+      if (child.isObject3D) {
+        child.rotateX(0.02);
+        child.rotateY(0.02);
+      }
+    });
+  });
+
+  return <group ref={groupRef}>{children}</group>;
+}
+
+function ReflectiveSphere() {
+  const tweakableProperties = useControls({
+    roughness: { value: 0.1, min: 0, max: 1 },
+    metalness: { value: 1, min: 0, max: 1 },
+  });
+
   return (
-    <Canvas>
+    <Sphere args={[1, 256, 256]}>
+      <meshStandardMaterial {...tweakableProperties} />
+    </Sphere>
+  );
+}
+
+function ThreeScene() {
+  const envMap = useEnvironment({ path: "/modern buildings" });
+
+  return (
+    <>
       <ambientLight />
-      <pointLight position={[5, 5, 5]} intensity={1} />
+      <pointLight position={[5, 5, 5]} />
       <pointLight position={[-3, -3, 2]} />
       <OrbitControls />
-    </Canvas>
+
+      <Environment map={envMap} background />
+
+      <CubeCamera frames={Infinity}>
+        {/* @ts-ignore */}
+        {(texture) => (
+          <>
+            <Environment map={texture} />
+            <ReflectiveSphere />
+          </>
+        )}
+      </CubeCamera>
+
+      <Rotator>
+        <Box position={[0, 0, 5]}>
+          <meshStandardMaterial color="red" />
+        </Box>
+
+        <Box position={[-1, 3, 2]}>
+          <meshStandardMaterial color="purple" />
+        </Box>
+      </Rotator>
+    </>
   );
 }
 
 function App() {
   return (
     <div className="App h-screen">
-      <ThreeScene />
+      <Canvas>
+        <ThreeScene />
+      </Canvas>
     </div>
   );
 }
