797000aa24128a33ac1c9bbe30cff192e25cc47c
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2022-10-20 20:04:12 +0200

    base-code-v2 but with eslint, prettier and prettier starting scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { OrbitControls } from "@react-three/drei";
+import { Box, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 
 function ThreeScene() {
   return (
     <>
-      <ambientLight />
-      <pointLight position={[5, 5, 5]} intensity={1} />
-      <pointLight position={[-3, -3, 2]} />
-      <OrbitControls />
+      <Box>
+        <meshStandardMaterial color="coral" />
+      </Box>
     </>
   );
 }
 
 function App() {
   return (
-    <div className="App h-screen">
+    <div className="App h-screen bg-gradient-to-br from-[#150f0b] to-[#010311]">
       <Canvas>
+        <ambientLight />
+        <pointLight position={[5, -2, 3]} />
+        <pointLight position={[-3, 4, -1]} />
+        <OrbitControls />
+
         <ThreeScene />
       </Canvas>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.log('Element with id "root" not found');
+}
