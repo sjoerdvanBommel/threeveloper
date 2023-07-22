8b7318abf46be9d32bfb6c56b5fbf35ce8d9ac7a
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2023-07-22 02:10:43 +0200

    add cookie clicker example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let cookiesCollected = 0;
+</script>
+
+<div>
+	<button on:click={() => cookiesCollected++}>Collect cookie</button>
+	<span>{cookiesCollected} cookies collected</span>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		width: 100vw;
+		height: 100vh;
+		justify-content: center;
+		align-items: center;
+	}
+
+	button {
+		border-radius: 9999px;
+		aspect-ratio: 1;
+		background-color: burlywood;
+		padding: 1rem;
+	}
+
+	span {
+		font-size: 3rem;
+	}
+</style>
